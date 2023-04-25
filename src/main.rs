e6295511729d8e21278290ffb7d4b70cdb4104f2
zayats1 <zaiats.bogdan@gmail.com>
2023-04-25 13:56:46 +0300

    MPL is the way

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #![no_std]
 #![no_main]
 
